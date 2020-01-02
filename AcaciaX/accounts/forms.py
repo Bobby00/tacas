e93388ae287d2bdaad30ae19e60d3d208bf9868c
bobby00 <edwarobert6@gmail.com>
2020-01-02 11:34:02 +0300

    push with db

--- AcaciaX/accounts/forms.py
+++ AcaciaX/accounts/forms.py
@@ -5,11 +5,10 @@ from django.contrib.auth.models import User
 from accounts.models import Profile
 
 class SignUpForm(UserCreationForm):
-    avatar = forms.ImageField()
     email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
     class Meta:
         model = User
-        fields = ('username', 'avatar', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
 
 class ProfileForm(UserCreationForm):
     class Meta:
