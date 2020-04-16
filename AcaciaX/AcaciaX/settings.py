c149bb24adfcf33352b76815b40546aca9221fbb
= <=>
2020-04-17 00:56:24 +0300

    update db settings locallh

--- AcaciaX/AcaciaX/settings.py
+++ AcaciaX/AcaciaX/settings.py
@@ -14,6 +14,15 @@ from decouple import config, Csv
 
 import os
 import dj_database_url
+from django.core.exceptions import ImproperlyConfigured
+
+def get_env_variable(name, default=None):
+    try:
+        return os.environ[name]
+    except KeyError:
+        if default is not None:
+            return default
+        raise ImproperlyConfigured(f'Missing environment variable {name}')
 
 
 
@@ -100,6 +109,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'postgres': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': get_env_variable('DB_NAME'),
+        'HOST': 'db',
+        'USER': get_env_variable('DB_USER'),
+        'PASSWORD': get_env_variable('DB_PASS')
     }
 }
 
